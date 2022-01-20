256a706b1f2745e796fc001067a588738f03be9a
James Wienecke <james.a.wienecke@gmail.com>
2022-01-20 14:35:11 -0600

    Caught up with Maven/dependency lecture.

--- src/main/java/GuavaTester.java
+++ src/main/java/GuavaTester.java
@@ -1,14 +1,38 @@
-import com.google.common.math.*;
+import com.google.common.base.Splitter;
+import com.google.common.math.DoubleMath;
+import com.google.common.math.IntMath;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GuavaTester {
     public static void main(String[] args) {
-        int bigNum = Integer.MAX_VALUE;
+        double num = Math.PI;
+        double butInt = 7;
 
-        System.out.println(bigNum + 1);
+        System.out.println(num);
+
+        System.out.println(DoubleMath.isMathematicalInteger(num));
+        System.out.println(DoubleMath.isMathematicalInteger(butInt));
+
+        int largeNum = Integer.MAX_VALUE;
+//        System.out.println(largeNum + 1);
         try {
-            System.out.println(IntMath.checkedAdd(bigNum, 1));
+            System.out.println(IntMath.checkedAdd(largeNum, 1));
         } catch (ArithmeticException e) {
-            System.err.println("OVERFLOW DETECTED OH NO");
+            System.err.println("That number is too big.");
+        }
+
+        System.out.println(largeNum);
+
+        String nameTag = "Hello,, ,My, Name,,, is,,, Joe!";
+        List<String> cutUpTag;
+        cutUpTag = Splitter.on(",").trimResults().omitEmptyStrings().splitToList(nameTag);
+
+        System.out.println(cutUpTag.size());
+
+        for (int i = 0; i < cutUpTag.size(); i ++) {
+            System.out.println(cutUpTag.get(i));
         }
     }
 }
