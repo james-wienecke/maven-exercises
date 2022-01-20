70eedc3dd3510e5cfed6e2a55e8ac435ebebfd4e
James Wienecke <james.a.wienecke@gmail.com>
2022-01-20 13:50:23 -0600

    Added Google Guava and began experimenting with its math methods.

---
+++ src/main/java/GuavaTester.java
@@ -0,0 +1,14 @@
+import com.google.common.math.*;
+
+public class GuavaTester {
+    public static void main(String[] args) {
+        int bigNum = Integer.MAX_VALUE;
+
+        System.out.println(bigNum + 1);
+        try {
+            System.out.println(IntMath.checkedAdd(bigNum, 1));
+        } catch (ArithmeticException e) {
+            System.out.println("OVERFLOW DETECTED OH NO");
+        }
+    }
+}
